416d40fcccb15c4c3fbc5949bdf857b85553b8ce
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-11 23:20:59 +0530

    Update __main__.py
    Hmmm

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -33,14 +33,15 @@ You can find the list of available commands with /help.
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the \
-things I can help you with.
+Hey there! My name is *{}*.
+I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of \
+the things I can help you with
 *Main* commands available:
  - /start: start the bot
  - /help: PM's you this message.
  - /settings:
-     - in PM: will send you your settings for all supported modules.
-     - in a group: will redirect you to pm, with all that chat's settings.
+   - in PM: will send you your settings for all supported modules.
+   - in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
