5ee86944973b8a1194bc5b4cb099adc97ad5dcd1
Skadi <63096193+Dank-del@users.noreply.github.com>
2020-06-11 18:23:26 +0530

    Update __main__.py
    I'm high

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -39,8 +39,8 @@ things I can help you with.
  - /start: start the bot
  - /help: PM's you this message.
  - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
+     - in PM: will send you your settings for all supported modules.
+     - in a group: will redirect you to pm, with all that chat's settings.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
